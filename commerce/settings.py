8501dd674360d89b3e66a03a26e646777deba1a6
Luana Palhares <luanaribeiropalhares@gmail.com>
2022-10-21 17:49:45 -0300

    settings.py

--- commerce/settings.py
+++ commerce/settings.py
@@ -129,5 +129,5 @@ USE_TZ = True
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-
+DISABLE_COLLECTSTATIC = 1
 django_heroku.settings(locals())
